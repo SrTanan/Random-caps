99039f1b66caf82a96a8afbcdfc02b5704705425
SrTanan <tanan7u7@outlook.es>
2020-08-26 17:23:54 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+extern crate rand;
+use std::io;
+use rand::Rng;
+
+fn main() {
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input); 
+        let mut byte_array = input.as_bytes();
+        
+        let mut rng = rand::thread_rng();
+
+        let mut output = String::new();
+        for i in 0..byte_array.len() {
+            if rng.gen::<f32>() < 0.5 {
+                output += &String::from_utf8(vec![byte_array[i]]).ok().unwrap();
+                continue;
+            }    
+
+            output += &String::from_utf8(vec![byte_array[i]]).ok().unwrap().to_uppercase();
+        }
+
+        println!("{}",output);
+    }
+}
